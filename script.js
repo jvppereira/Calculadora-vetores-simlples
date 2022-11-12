898f400616fb3ced1d3126d9dbaed027d2a92160
jvppereira <jvppereira1@gmail.com>
2022-11-12 01:54:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+e.preventDefault();
+const inputvetor1 = e.target.querySelector('#vetor1');
+const inputvetor2 = e.target.querySelector('#vetor2');
+
+const vetor_1 = Number(inputvetor1.value)});
+console.log(vetor_1)
